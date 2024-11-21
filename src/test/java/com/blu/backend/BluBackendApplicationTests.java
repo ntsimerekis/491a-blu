e69dcb3864c8f2d63ba048201ed31cbc4150e0cb
Nicholas Tsimerekis <ntsimerekis@gmail.com>
2024-11-21 13:30:18 -0800

    my first commit ;)

---
+++ src/test/java/com/blu/backend/BluBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blu.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
