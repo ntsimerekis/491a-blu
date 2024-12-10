e5de83fd3efd64a5307e65f9f735a12f5d2feec6
Nicholas Tsimerekis <ntsimerekis@gmail.com>
2024-12-10 01:53:08 +0000

    Java Web Token Success!

--- src/main/java/com/blu/backend/BluBackendApplication.java
+++ src/main/java/com/blu/backend/BluBackendApplication.java
@@ -2,8 +2,12 @@ package com.blu.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.blu.*"})
+@EntityScan("com.blu.*")
+@EnableJpaRepositories("com.blu.auth")
 public class BluBackendApplication {
 
     public static void main(String[] args) {
