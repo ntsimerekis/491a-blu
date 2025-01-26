fa984455549900d8c77237bd8e03a3c4248c7d5d
Nicholas Tsimerekis <ntsimerekis@gmail.com>
2025-01-26 06:31:15 +0000

    Got stuff to work with CORS

--- src/main/java/com/blu/auth/AuthenticationController.java
+++ src/main/java/com/blu/auth/AuthenticationController.java
@@ -1,13 +1,11 @@
 package com.blu.auth;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/auth")
+@CrossOrigin
 @RestController
+@RequestMapping("/auth")
 public class AuthenticationController {
     private final JwtService jwtService;
 
